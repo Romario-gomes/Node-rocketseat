1643ee0dde9bfa6010ee37d428a755af311f6ac6
Romario Work <romario.g@estrela10.com.br>
2021-09-27 09:35:01 -0300

    buscando statement conta bancária

--- primeiro-projeto/src/index.js
+++ primeiro-projeto/src/index.js
@@ -1,10 +1,11 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
 /**
  *  cpf - string
  *  name - string
@@ -13,18 +14,32 @@ const costumers = [];
  *  */
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
 
-    costumers.push({
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+
+    );
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "Customer already exists!" });
+    }
+
+    customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4,
         statement: []
     });
 
     return response.status(201).send();
 });
 
+app.get("/statement/:cpf", (request, response ) =>{ 
+    const { cpf } = request.params;
+    
 
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statement);
+});
 
 app.listen(3333);
