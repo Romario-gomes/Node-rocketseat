ecff64e89f726c52cca33b54db3a25e2084ece11
Romario Work <romario.g@estrela10.com.br>
2021-10-08 19:07:37 -0300

    criação de categoria e trabalhando com debbuger

--- Chapter II/rentalx/src/server.ts
+++ Chapter II/rentalx/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
-
+import { categoriesRoutes } from "./routes/categories.routes";
 const app = express();
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.get("/", (request, response) => {
     response.json({message: "Hello World"});
 });
+
+app.post("/courses", (request, response) =>{
+    const { name } = request.body;
+
+    return response.json({ name });
+})
 
 app.listen(3333, () => console.log("Server is running"));
