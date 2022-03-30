fe409ea5abbbaccabf7449c66c3234eaac5b60f0
Romario Work <romario.g@estrela10.com.br>
2022-03-29 23:09:46 -0300

    Feat: Add Bcryptjs

--- Chapter II/rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ Chapter II/rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
+import { hash } from "bcryptjs";
+ 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRespository } from "../../repositories/IUsersRepository";
 
@@ -10,9 +13,14 @@ class CreateUserUseCase {
     ) {}
 
     async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
-            name, email, password, driver_license
-        })
+            name, 
+            email, 
+            password: passwordHash, 
+            driver_license
+        });
     }
 }
 
